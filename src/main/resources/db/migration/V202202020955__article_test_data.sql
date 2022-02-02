f8766c1588e76f240b1c57e3cc96313856df5a56
OlegHolovko <wodan78@gmail.com>
2022-02-02 10:09:34 +0200

    Add article test data migration

---
+++ src/main/resources/db/migration/V202202020955__article_test_data.sql
@@ -0,0 +1,3 @@
+insert into articles (id, name) values (3, 'Test migration article');
+insert into articles (id, name) values (4, 'Test migration article2');
+insert into articles (name) values ('Test migration article without id');
